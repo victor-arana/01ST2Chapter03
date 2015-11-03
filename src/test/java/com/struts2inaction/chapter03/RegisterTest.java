50792ca96adfcdbccf6b4e713df251404aa8af3a
Victor Jose Arana Rodriguez <victor_arana@hotmail.com>
2015-11-02 19:21:14 -0600

    JUnit Test added

--- src/test/java/com/struts2inaction/chapter03/RegisterTest.java
+++ src/test/java/com/struts2inaction/chapter03/RegisterTest.java
@@ -1,9 +1,22 @@
 package com.struts2inaction.chapter03;
 
 import org.apache.struts2.StrutsTestCase;
-import org.junit.Test;
+
+import com.opensymphony.xwork2.ActionProxy;
 
 public class RegisterTest extends StrutsTestCase {
-	
+
+	public void testUserNameErrorMessage() throws Exception{
+		
+		request.setParameter("username", "");
+		request.setParameter("password", "password");
+		request.setParameter("portfolioName", "value");
+		
+		ActionProxy actionProxy = getActionProxy("/Register");		
+		Register registerAction = (Register) actionProxy.getAction();		
+		actionProxy.execute();
+		
+		assertTrue("Problem There were no errors present in fieldErrors but there should have been one error present", registerAction.getFieldErrors().size() == 1);
+	}
 
 }
